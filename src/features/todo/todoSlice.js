08ba251bde644883e6e3f3a0be2f526dd525086f
Vidhi Singh <vidhisingh1956@gmail.com>
2025-01-24 18:26:56 +0530

    update To-Do and add reducer to the store

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -6,9 +6,10 @@ const initialState={
 // slice is a bigger form of Reducer
 // reducer is a function
 export const todoSlice = createSlice({
-    name:'todo',
+    name:'todo', // reflect in the redux dev extension
     initialState,
     reducers:{
+        // state has curretn state and action is the input
         addToDo:(state,action)=>{
             const todo={
                 id:nanoid(),
@@ -16,8 +17,26 @@ export const todoSlice = createSlice({
             }
             state.todos.push(todo)
         },
-        removeToDo:()=>{
+        removeToDo:(state, action)=>{
+
+            state.todos = state.todos.filter((todo)=>
+                todo.id !== action.payload
+            )
             
+        },
+        updateToDo:(state,action)=>{
+            state.todos = state.todos.map((todo)=>
+                //rtk supports immutable updates if i do todo.text = action.payload thingy then it mutates the state, so we would have to create a new object by using the spread operator
+                (todo.id===action.payload.id)?{
+                    ...todo,text: action.payload.text
+                }:todo
+            )
         }
     }
 })
+
+// extracts and exports the action creators (addToDo, removeToDo, updateToDo) so that they can be used inside React components.
+export const {addToDo,removeToDo,updateToDo} = todoSlice.actions
+
+//  exports the reducer function so that it can be added to the Redux store.
+export default todoSlice.reducer
